7388e7a62c7e6552b366821309d52ef170114e3f
skynet-soulja <martinwillnorman@protonmail.com>
2020-11-08 10:24:28 -0500

    type server.ts

--- server.ts
+++ server.ts
@@ -1,32 +1,32 @@
-import express from "express";
-import path from "path";
-import dotenv from "dotenv";
-dotenv.config();
-import nodemailer from "nodemailer";
-import createPDF from "./createPDF";
+import express, { Application, Request, Response, NextFunction } from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
+import nodemailer from 'nodemailer'
+import createPDF from './createPDF'
 
-const app = express();
+const app: Application = express()
 
-app.use(express.static(path.join(__dirname, "app/build")));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'app/build')))
+app.use(express.json())
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
     user: process.env.G_USER,
     pass: process.env.G_PW,
   },
-});
+})
 
 transporter.verify((error, success) => {
-  error ? console.log(error) : console.log(success);
-});
+  error ? console.log(error) : console.log(success)
+})
 
-app.post("/api/send", async (request, response, next) => {
+app.post('/api/send', async (request: Request, response: Response, next: NextFunction) => {
   try {
-    const pdf = await createPDF(request.body);
+    const pdf = await createPDF(request.body)
 
-    const { sendTo, invoiceNumber } = request.body;
+    const { sendTo, invoiceNumber } = request.body
 
     const email = {
       to: sendTo,
@@ -39,26 +39,27 @@ app.post("/api/send", async (request, response, next) => {
           content: pdf,
         },
       ],
-    };
+    }
 
     transporter.sendMail(email, (error) => {
       error
         ? response.status(500).json({
-            message: "Email failed to send!",
+            message: 'Email failed to send!',
           })
         : response.status(200).json({
-            message: "Email successfuly sent!",
-          });
-    });
+            message: 'Email successfuly sent!',
+          })
+    })
   } catch (error) {
-    next(error);
+    console.log(error)
+    next(error)
   }
-});
+})
 
-app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "app/build/index.html"));
-});
+app.get('*', (request: Request, response: Response) => {
+  response.sendFile(path.join(__dirname, 'app/build/index.html'))
+})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
